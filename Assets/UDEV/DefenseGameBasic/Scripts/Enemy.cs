0b8b2cf5af73acf1e4438b9dede453f8bf865fd9
KhanhZuka <nguyennamkhanh2382005@gmail.com>
2026-09-10 14:49:34 +0700

    Add Player Weapon Script
    Add Player Weapon Script, Layer setup

--- Assets/UDEV/DefenseGameBasic/Scripts/Enemy.cs
+++ Assets/UDEV/DefenseGameBasic/Scripts/Enemy.cs
@@ -34,7 +34,9 @@ namespace UDEV.DefenseBasic
         {
             if(IsComponentsNull()) return;
 
-            if (Vector2.Distance(m_player.transform.position, transform.position) <= atkDistance) {
+            float distToPlayer = Vector2.Distance(m_player.transform.position, transform.position);
+
+            if ( distToPlayer <= atkDistance) {
                 m_anim.SetBool(Const.ATTACK_ANIM, true);
                 m_rb.velocity = Vector2.zero; 
             }
@@ -42,6 +44,14 @@ namespace UDEV.DefenseBasic
             {
                 m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
             }
+        }
+
+        public void Die()
+        {
+            if (IsComponentsNull()) return;
+            m_anim.SetTrigger(Const.DEAD_ANIM);
+            m_rb.velocity = Vector2.zero;
+            gameObject.layer = LayerMask.NameToLayer(Const.DEAD_ANIM);
         }
     }
 }
